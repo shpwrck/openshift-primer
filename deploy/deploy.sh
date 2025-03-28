df4a5829cf19d2c7fc20e9d8f1fbca94e55cf740
Jason Skrzypek <jankoszy@gmail.com>
2025-03-28 14:05:55 -0400

    Updated Deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
+
+helm repo add rhpds https://rhpds.github.io/showroom-deployer
+helm repo update
 helm upgrade --install myshowroom rhpds/showroom-single-pod \
     --values single-pod-values.yaml
-    #--set deployer.domain=apps.ms01.k8socp.com \
-    #--set general.guid=00001 \
-    #--set documentation.repoUrl=https://github.com/shpwrck/workshop-example
