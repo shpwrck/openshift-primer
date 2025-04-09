80c03db0c1375a4a850815c33c63034e48526a02
Jason Skrzypek <jankoszy@gmail.com>
2025-04-09 15:23:34 -0400

    MC

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,4 +3,4 @@
 helm repo add rhpds https://rhpds.github.io/showroom-deployer
 helm repo update
 helm upgrade --install myshowroom rhpds/showroom-single-pod \
-    --values single-pod-values.yaml
+    --values deploy/single-pod-values.yaml
